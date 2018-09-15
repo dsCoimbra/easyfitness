23b31b4867398429743a6bde340c13d99ea1c5fd
David de Souza Coimbra <332304@grupofolha.intranet>
2018-09-14 22:00:19 -0300

    Model e Controller criados - Laravel

---
+++ app/Http/Controllers/IndicationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Indication;
+use Illuminate\Http\Request;
+
+class IndicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Indication  $indication
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Indication $indication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Indication  $indication
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Indication $indication)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Indication  $indication
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Indication $indication)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Indication  $indication
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Indication $indication)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PhysicalAssessmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PhysicalAssessment;
+use Illuminate\Http\Request;
+
+class PhysicalAssessmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PhysicalAssessment  $physicalAssessment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PhysicalAssessment $physicalAssessment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PhysicalAssessment  $physicalAssessment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PhysicalAssessment $physicalAssessment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PhysicalAssessment  $physicalAssessment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PhysicalAssessment $physicalAssessment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PhysicalAssessment  $physicalAssessment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PhysicalAssessment $physicalAssessment)
+    {
+        //
+    }
+}
